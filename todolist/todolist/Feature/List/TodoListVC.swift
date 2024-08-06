36bf22bf2046e1ceefa6d543e7c3d5279b4b0739
Agha Maulana <amrikudou@gmail.com>
2024-08-06 11:41:04 +0700

    init tableview

--- todolist/todolist/Feature/List/TodoListVC.swift
+++ todolist/todolist/Feature/List/TodoListVC.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class TodoListVC: UIViewController {
-    
+    internal lazy var tableView: UITableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +19,59 @@ class TodoListVC: UIViewController {
 
 extension TodoListVC {
     private func setupView() {
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupNavbar()
+        setupTableView()
+    }
+    
+    private func setupNavbar() {
+        let label: UILabel = UILabel()
+        label.textColor = .black
+        label.text = "To Do List"
+        label.font = .boldSystemFont(ofSize: 20)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: label)
+        
+        let button: UIButton = UIButton()
+        button.addTarget(self, action: #selector(rightButtonAction), for: .touchUpInside)
+        var configuration: UIButton.Configuration = .plain()
+        configuration.background.backgroundColor = .systemBlue
+        configuration.background.cornerRadius = 4
+        configuration.title = "+ New Task"
+        configuration.baseForegroundColor = .white
+        configuration.contentInsets = .init(top: 4, leading: 12, bottom: 4, trailing: 12)
+        button.configuration = configuration
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: button)
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .systemGray6
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    @objc private func rightButtonAction(_ sender: UIBarButtonItem) {
+        print("Open form page")
+    }
+}
+
+extension TodoListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
